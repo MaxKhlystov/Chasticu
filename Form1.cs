c017b752905f09d93c3b30f10478b97359f37f7c
stud <stud@I21013406094>
2025-03-19 13:16:55 +0800

    добавил частицы

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,52 @@ namespace Частицы
 {
     public partial class Form1 : Form
     {
+
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
+            }
+
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                var directionInRadians = particle.Direction / 180 * Math.PI;
+                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+            }
+        }
+
+        private void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g);
+            }
+            picDisplay.Invalidate();
         }
     }
 }
---
+++ Particle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Частицы
+{
+    class Particle
+    {
+        public int Radius; 
+        public float X; 
+        public float Y;
+
+        public float Direction;
+        public float Speed;
+
+        public static Random rand = new Random();
+
+        public Particle()
+        {
+       
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(100);
+            Radius = 2 + rand.Next(10);
+        }
+        public void Draw(Graphics g)
+        {
+            var b = new SolidBrush(Color.Black);
+
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+            b.Dispose();
+        }
+    }
+}
