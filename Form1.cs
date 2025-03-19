82e201719ebd24c8ae0844a2e6cffa9334f6ebd5
Densi88 <Ignatiev1904.ph@mail.ru>
2025-03-19 14:50:54 +0800

    Посмотрел клик мышки

--- Form1.cs
+++ Form1.cs
@@ -14,18 +14,13 @@ namespace Частицы
     {
 
         List<Particle> particles = new List<Particle>();
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-            for (var i = 0; i < 500; ++i)
-            {
-                var particle = new Particle();
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                particles.Add(particle);
-            }
-
         }
 
         private void picDisplay_Click(object sender, EventArgs e)
@@ -34,11 +29,43 @@ namespace Частицы
         }
         private void UpdateState()
         {
+            //по клику взрывы как фейверк
+            /*foreach (var particle in particles)
+            {
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }*/
             foreach (var particle in particles)
             {
-                var directionInRadians = particle.Direction / 180 * Math.PI;
-                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                particle.Life -= 1; 
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) 
+                {
+                    var particle = new ParticleColorful();
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else break; 
             }
         }
 
@@ -54,10 +81,30 @@ namespace Частицы
             UpdateState();
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White);
+                g.Clear(Color.Black);
                 Render(g);
             }
             picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
+        }
+
+        private void picDisplay_MouseClick(object sender, MouseEventArgs e)
+        {
+            //по клику взрывы как фейверк
+            /*for (var i = 0; i < 500; ++i)
+            {
+                var particle = new ParticleColorful();
+                particle.FromColor = Color.Yellow;
+                particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                particle.X = MousePositionX;
+                particle.Y = MousePositionY;
+                particles.Add(particle);
+            }*/
         }
     }
 }
--- Particle.cs
+++ Particle.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace Частицы
 {
-    class Particle
+    public class Particle
     {
-        public int Radius; 
-        public float X; 
+        public float Life;
+        public int Radius;
+        public float X;
         public float Y;
 
         public float Direction;
@@ -20,17 +21,51 @@ namespace Частицы
 
         public Particle()
         {
-       
-            Direction = rand.Next(360);
-            Speed = 1 + rand.Next(100);
-            Radius = 2 + rand.Next(10);
-        }
-        public void Draw(Graphics g)
-        {
-            var b = new SolidBrush(Color.Black);
 
-            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+        public virtual void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+            int alpha = (int)(k * 255);
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
             g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
+    }
+    public class ParticleColorful : Particle
+    {
+        // два новых поля под цвет начальный и конечный
+        public Color FromColor;
+        public Color ToColor;
+
+        // для смеси цветов
+        public static Color MixColor(Color color1, Color color2, float k)
+        {
+            return Color.FromArgb(
+                (int)(color2.A * k + color1.A * (1 - k)),
+                (int)(color2.R * k + color1.R * (1 - k)),
+                (int)(color2.G * k + color1.G * (1 - k)),
+                (int)(color2.B * k + color1.B * (1 - k))
+            );
+        }
+
+        // ну и отрисовку перепишем
+        public override void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+
+            // так как k уменьшается от 1 до 0, то порядок цветов обратный
+            var color = MixColor(ToColor, FromColor, k);
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
             b.Dispose();
         }
     }
